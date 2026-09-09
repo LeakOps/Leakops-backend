3f908630c41a19870d3d7408ec2ff5cb115fa7d8
ASPCODES <adityawork0811@gmail.com>
2026-09-09 23:53:48 +0530

    Update go.mod and go.sum to include new dependencies and remove unused ones; refactor main.go for improved structure

--- server/main.go
+++ server/main.go
@@ -1,1 +1,20 @@
 package main
+
+import(
+	"log"
+
+	"Leakops-backend/internal/config"
+	"Leakops-backend/internal/db"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	// Load env from config
+	cfg := config.LoadConfig()
+
+	// Connect to database + auto-migrate models
+	database := db.ConnectDB(cfg.DatabaseURL)
+}
