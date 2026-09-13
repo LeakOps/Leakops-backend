df97268968579db5f6ff53121d4ab1d3ced98cf9
ASPCODES <adityawork0811@gmail.com>
2026-09-13 16:06:30 +0530

    Implement Aes-265 encryption

--- internal/utils/encryption.go
+++ internal/utils/encryption.go
@@ -1,10 +1,11 @@
 package utils
 
-import(
+import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"io"
 )
 
@@ -31,3 +32,33 @@ func Encrypt(plainText string, key string) (string, error) {
 }
 
 // Decrypt takes base64 encoded ciphertext and a 32-byte key, returns plain text
+func Decrypt(cipherText string, key string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	nonce, cipherBytes := data[:nonceSize], data[nonceSize:]
+	plainBytes, err := gcm.Open(nil, nonce, cipherBytes, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainBytes), nil
+}
+
